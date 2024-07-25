e3081fd1f18a77d6070182ccbe4c3cfb2bb4536e
Beefiker <beefiker@gmail.com>
2024-07-25 11:22:13 +0900

    FIX: build error

---
+++ packages/ui/src/jongmin-component.tsx
@@ -0,0 +1,8 @@
+export const JongminComponent = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>JongminComponent Component</h1>
+      {children}
+    </div>
+  );
+};
